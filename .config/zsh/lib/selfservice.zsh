eeaa54993a59063d3364a79028814d04db95fcee
Konstantin Demin <rockdrilla@gmail.com>
2021-03-21 22:14:40 +0300

    dotfiles maintenance in subshell

--- .config/zsh/lib/selfservice.zsh
+++ .config/zsh/lib/selfservice.zsh
@@ -4,6 +4,13 @@ dotfiles-update() {
     "${ZSHU[d_zdot]}/.config/dotfiles/install.sh"
 }
 
+dotfiles-git() { (
+    cd "${ZSHU[d_zdot]}/"
+    export GIT_DIR="${ZSHU[d_zdot]}/.config/dotfiles/repo.git"
+    export GIT_WORK_TREE="${ZSHU[d_zdot]}"
+    zsh -i
+) }
+
 z-update() {
     dotfiles-update
 }
