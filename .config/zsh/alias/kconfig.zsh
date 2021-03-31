100187ca046f488a6341815dac36fac706f4514a
Konstantin Demin <rockdrilla@gmail.com>
2021-03-31 08:54:40 +0300

    add kconfig helper functions

---
+++ .config/zsh/alias/kconfig.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+kconf-set() {
+    local n=$1 v=$2 ; shift 2
+    grep -ElZ "^((CONFIG_)?$n=|# (CONFIG_)?$n is not set)" "$@" \
+    | xargs -0 -r sed -i -E -e "s/^(((CONFIG_)?$n)=.+|# ((CONFIG_)?$n) is not set)\$/\\2\\4=$v/"
+}
+
+kconf-unset() {
+    local n=$1 ; shift
+    grep -ElZ "^(CONFIG_)?$n=" "$@" \
+    | xargs -0 -r sed -i -E -e "s/^((CONFIG_)?$n)=.+\$/# \\1 is not set/"
+}
