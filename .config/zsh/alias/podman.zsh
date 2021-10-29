e1ac7abd9296d13a5d706940880614dc351150e6
Konstantin Demin <rockdrilla@gmail.com>
2021-10-29 22:35:35 +0300

    podman run: provide TERM to container

--- .config/zsh/alias/podman.zsh
+++ .config/zsh/alias/podman.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-run() { command podman run --network host --rm -it "$@" ; }
+run() { command podman run ${TERM:+-e "TERM=$TERM"} --network host --rm -it "$@" ; }
 run-sh() { run --entrypoint='["/bin/sh"]' --user=0:0 "$@" ; }
