aea9334649db558eb0e2cd5b9610d38ea24de0bb
Konstantin Demin <rockdrilla@gmail.com>
2024-02-13 18:54:46 +0300

    zsh: update

--- .config/zsh/alias/directories.zsh
+++ .config/zsh/alias/directories.zsh
@@ -14,3 +14,18 @@ alias 1='cd -'
 for (( i=2 ; i < 10 ; i++ )) ; do
     alias $i="cd -$i"
 done ; unset i
+
+## "Go to Dir" - create path if missing
+gd() {
+    [ $# -lt 2 ] || echo "# gd() takes no more than one argument, seen instead: $#" >&2
+
+    case "$#" in
+    0 ) cd ;;
+    * )
+        if ! [ -d "$1" ] ; then
+            mkdir -p "$1" || return $?
+        fi
+        cd "$1"
+    ;;
+    esac
+}
