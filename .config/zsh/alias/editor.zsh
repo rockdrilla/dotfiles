95e0069fbecc4b6ad2a33ec3144519029481e0f4
Konstantin Demin <rockdrilla@gmail.com>
2026-03-14 10:50:42 +0300

    zsh: fix editor
    triggered when e-wrapper is installed

--- .config/zsh/alias/editor.zsh
+++ .config/zsh/alias/editor.zsh
@@ -4,7 +4,7 @@ EDITOR=$(z-alt-find 'e|vim.basic|vim.nox|vim.tiny|vim|mcedit|nano')
 if [ -n "${EDITOR}" ] ; then
     export EDITOR
     z-alt-set-static e "${EDITOR}"
-    alias e="${EDITOR:t} "
+    alias e="command ${EDITOR:t} "
 else
     unset EDITOR
 fi
