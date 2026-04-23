e2ce632efb772552d2fd31d31737376483ea3927
Konstantin Demin <rockdrilla@gmail.com>
2026-04-23 18:17:04 +0300

    dotfiles: initial tmux support

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -61,6 +61,18 @@ z-set-tmpdir() {
 }
 z-set-tmpdir "${TMPDIR:=/tmp}"
 
+: ${XDG_CONFIG_HOME:=${HOME}/.config}
+: ${XDG_CACHE_HOME:=${HOME}/.cache}
+: ${XDG_DATA_HOME:=${HOME}/.local/share}
+: ${XDG_RUNTIME_DIR:=${TMPDIR}}
+: ${XDG_DATA_DIRS:=/usr/local/share:/usr/share}
+: ${XDG_CONFIG_DIRS:=/etc/xdg}
+
+typeset -x -m 'XDG*'
+
+typeset -x -T XDG_DATA_DIRS    xdg_data_dirs
+typeset -x -T XDG_CONFIG_DIRS  xdg_config_dirs
+
 ZSHU[uname]=$(uname -s 2>/dev/null)
 ZSHU[uname]=${ZSHU[uname]:l}
 
---
+++ .config/zsh/env/tmux.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+TMUX_TMPDIR=${XDG_RUNTIME_DIR}
+
+typeset -x -m 'TMUX_*'
